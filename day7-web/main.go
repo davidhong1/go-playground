376240531cde76ad59720888a637e39f6f7ef085
DavidHong <david.hong@klook.com>
2024-06-13 17:11:17 +0800

    day7 web add recovery

--- day7-web/main.go
+++ day7-web/main.go
@@ -1,50 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"text/template"
-	"time"
 
 	"github.com/davidhong1/go-playground/day7-web/gee"
 )
 
-type student struct {
-	Name string
-	Age  int8
-}
-
-func FormatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
-}
-
 func main() {
-	r := gee.New()
-	r.Use(gee.Logger())
-	r.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsDate,
-	})
-	r.LoadHTMLGlob("day7-web/templates/*")
-	r.Static("/assets", "./day7-web/static")
-
-	stu1 := &student{Name: "Geektutu", Age: 20}
-	stu2 := &student{Name: "Jack", Age: 22}
+	r := gee.Default()
 	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", nil)
-	})
-	r.GET("/students", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
-			"title":  "gee",
-			"stuArr": [2]*student{stu1, stu2},
-		})
+		c.String(http.StatusOK, "Hello Geektutu\n")
 	})
 
-	r.GET("/data", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
-			"title": "gee",
-			"now":   time.Now(),
-		})
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[1000])
 	})
 
 	r.Run(":9999")
---
+++ day7-web/main6.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+	"time"
+
+	"github.com/davidhong1/go-playground/day7-web/gee"
+)
+
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
+func main6() {
+	r := gee.New()
+	r.Use(gee.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("day7-web/templates/*")
+	r.Static("/assets", "./day7-web/static")
+
+	stu1 := &student{Name: "Geektutu", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
+			"title":  "gee",
+			"stuArr": [2]*student{stu1, stu2},
+		})
+	})
+
+	r.GET("/data", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
+			"title": "gee",
+			"now":   time.Now(),
+		})
+	})
+
+	r.Run(":9999")
+}
